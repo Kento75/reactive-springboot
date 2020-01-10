f6d7a712cdddc4d6008323d5b57c28cbe257d05b
Kento Takano <kento2github@gmail.com>
2020-01-10 09:58:41 +0900

    Added Mono simple Test Case

--- learn-reactivespring/src/test/java/com/learnreactivespring/fluxandmonoplayground/FluxAndMonoTest.java
+++ learn-reactivespring/src/test/java/com/learnreactivespring/fluxandmonoplayground/FluxAndMonoTest.java
@@ -3,10 +3,12 @@ package com.learnreactivespring.fluxandmonoplayground;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 public class FluxAndMonoTest {
 
+  /* Flux テストケース */
   @Test
   @DisplayName("簡単な Pub/Sub")
   public void fluxTest() {
@@ -74,6 +76,25 @@ public class FluxAndMonoTest {
     StepVerifier.create(stringFlux)
         .expectNextCount(3)
         .expectErrorMessage("Exception Occurred")
+        .verify();
+  }
+
+  /* Mono テストケース */
+
+  @Test
+  @DisplayName("シンプルなMono テスト")
+  public void monoTest() {
+    Mono<String> stringMono = Mono.just("Spring");
+
+    StepVerifier.create(stringMono.log()).expectNext("Spring").verifyComplete();
+  }
+
+  @Test
+  @DisplayName("シンプルなMono テスト - エラー")
+  public void monoTest_Error() {
+
+    StepVerifier.create(Mono.error(new RuntimeException("Exception Occurred")).log())
+        .expectError(RuntimeException.class)
         .verify();
   }
 }
