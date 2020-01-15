d10aa324314c957b1fb4778ac3bf21c33be18e2c
Kento Takano <kento2github@gmail.com>
2020-01-15 19:54:28 +0900

    Added get all test case for ItemControllerTest

--- learn-reactivespring/src/main/java/com/learnreactivespring/initialize/ItemDataInitializer.java
+++ learn-reactivespring/src/main/java/com/learnreactivespring/initialize/ItemDataInitializer.java
@@ -4,6 +4,7 @@ import com.learnreactivespring.document.Item;
 import com.learnreactivespring.repository.ItemReactiveRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 
@@ -11,10 +12,10 @@ import java.util.Arrays;
 import java.util.List;
 
 @Component
+@Profile("!test") // test以外
 public class ItemDataInitializer implements CommandLineRunner {
 
-  @Autowired
-  ItemReactiveRepository itemReactiveRepository;
+  @Autowired ItemReactiveRepository itemReactiveRepository;
 
   // ブートストラップ
   @Override
@@ -25,21 +26,24 @@ public class ItemDataInitializer implements CommandLineRunner {
 
   public List<Item> data() {
 
-    return Arrays.asList(new Item(null, "Samsung TV", 399.99),
-                         new Item(null, "LG TV", 329.99),
-                         new Item(null, "AppliWatch", 349.99),
-                         new Item("ABC", "Beats HeadPhones", 19.99));
+    return Arrays.asList(
+        new Item(null, "Samsung TV", 399.99),
+        new Item(null, "LG TV", 329.99),
+        new Item(null, "AppliWatch", 349.99),
+        new Item("ABC", "Beats HeadPhones", 19.99));
   }
 
   // 初期データ作成
   private void initialDataSetUp() {
 
-    itemReactiveRepository.deleteAll()
+    itemReactiveRepository
+        .deleteAll()
         .thenMany(Flux.fromIterable(data()))
-            .flatMap(itemReactiveRepository::save)
-            .thenMany(itemReactiveRepository.findAll())
-        .subscribe((item -> {
-            System.out.println("Item inserted from CommandLineRunner : " + item);
-        }));
+        .flatMap(itemReactiveRepository::save)
+        .thenMany(itemReactiveRepository.findAll())
+        .subscribe(
+            (item -> {
+              System.out.println("Item inserted from CommandLineRunner : " + item);
+            }));
   }
 }
